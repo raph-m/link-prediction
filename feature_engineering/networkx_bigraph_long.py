b1e98f1abe2c6014f742b8fa9353b38864eb7bef
raph-m <raphael.montaud@riminder.net>
2018-03-08 23:06:36 +0100

    compute bigraph_long on CE

--- feature_engineering/networkx_bigraph_long.py
+++ feature_engineering/networkx_bigraph_long.py
@@ -31,7 +31,8 @@ id2 = training['id2'].values
 
 # placeholder for feature
 n = len(id1)
-n = 1000
+print("start computing for training: ")
+print("size of data to process: " + str(n))
 katz = np.zeros(n)
 katz_2 = np.zeros(n)
 breaking_point = 10
@@ -39,17 +40,17 @@ beta = 0.98
 beta_2 = 0.9
 
 
-def work(i0=None, n=None, id1=None, id2=None, df=None, training=True):
+def work(i0=None, n=None, is_training=True):
 
     G = nx.Graph()
     G.add_nodes_from(nodes.index.values)
-    G.add_edges_from(zip(df[df["target"] == 1]["id1"], df[df["target"] == 1]["id2"]))
+    G.add_edges_from(zip(training[training["target"] == 1]["id1"], training[training["target"] == 1]["id2"]))
 
     ans = np.zeros(n)
     ans_2 = np.zeros(n)
     for i in range(i0, i0+n):
-        if training:
-            if df.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
+        if is_training:
+            if training.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
                 G.remove_edge(id1[i], id2[i])
 
         katz_acc = 0.0
@@ -68,14 +69,19 @@ def work(i0=None, n=None, id1=None, id2=None, df=None, training=True):
             ans[i] = -1
             ans_2[i] = -1
 
-        if training:
-            if df.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
+        if is_training:
+            if training.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
                 G.add_edge(id1[i], id2[i])
 
         ans[i] = katz_acc
         ans_2[i] = katz_2_acc
 
-    return ans, ans_2
+    return ans, ans_2, i0
+
+
+def callback(r):
+    ans, ans_2, i0 = r
+    print(i0)
 
 # computing features for training set
 
@@ -83,23 +89,21 @@ pool = Pool(processes=2)
 print("starting pool...")
 import time
 start = time.time()
-n_tasks = 64
+n_tasks = 512
 tasks = []
+step = int(n / n_tasks)
 for i0 in range(n_tasks):
     kwds = {
-        "i0": i0 * n / n_tasks,
-        "n": n / n_tasks,
-        "id1": id1,
-        "id2": id2,
-        "df": training,
-        "training": True
+        "i0": i0 * step,
+        "n": step,
+        "is_training": True
     }
-    tasks.append(pool.apply_async(work, kwds=kwds))
+    tasks.append(pool.apply_async(work, kwds=kwds, callback=callback))
 pool.close()
 pool.join()
 for i in range(n_tasks):
-    katz[i * len(id1) / n_tasks: (i+1) * len(id1) / n_tasks],\
-        katz_2[i * len(id1) / n_tasks: (i+1) * len(id1) / n_tasks] = tasks[i].get()
+    katz[i * step: (i + 1) * step],\
+        katz_2[i * step: (i+1) * step], _ = tasks[i].get()
 
 end = time.time()
 print(end-start)
@@ -109,37 +113,40 @@ training["katz_2"] = katz_2
 
 
 # IDs for testing set
+print("start computing for training: ")
 id1 = testing['id1'].values
 id2 = testing['id2'].values
 
 # placeholder for feature
 n = len(id1)
+print("size of data to process: " + str(n))
 
 katz = np.zeros(n)
 katz_2 = np.zeros(n)
 
-# computing features for testing set
-pool = Pool()
+pool = Pool(processes=2)
+print("starting pool...")
+n_tasks = 512
 tasks = []
-for i in range(len(id1)):
+step = int(n / n_tasks)
+for i0 in range(n_tasks):
     kwds = {
-        "i": i,
-        "id1": id1,
-        "id2": id2,
-        "df": training,
-        "training": True
+        "i0": i0 * step,
+        "n": step,
+        "is_training": False
     }
-    tasks.append(pool.apply_async(work, kwds=kwds))
+    tasks.append(pool.apply_async(work, kwds=kwds, callback=callback))
 pool.close()
 pool.join()
-for i in range(len(katz)):
-    katz[i], katz_2[i] = tasks[i].get()
+for i in range(n_tasks):
+    katz[i * step: (i + 1) * step],\
+        katz_2[i * step: (i+1) * step], _ = tasks[i].get()
 
 # add feature to data-frame
 testing["katz"] = katz
 testing["katz_2"] = katz_2
 
-
+print("done, saving data")
 # save data-frame
 training.to_csv(path_to_data + "training_features.txt")
 testing.to_csv(path_to_data + "testing_features.txt")
