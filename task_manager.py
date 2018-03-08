44b0c28006b54fbaacd4934a30c750181fb7c158
raph-m <raphael.montaud@riminder.net>
2018-03-08 23:31:41 +0100

    new bigraph_long without parallel

--- task_manager.py
+++ task_manager.py
@@ -2,6 +2,6 @@ import time
 
 start = time.time()
 print("networkx_bigraph_long:")
-import feature_engineering.networkx_bigraph_long
+import feature_engineering.networkx_bigraph_long2
 end = time.time()
 print("done in: "+str(end-start))
