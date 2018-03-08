f281d5372af79c8fd208192f7408556db8ab8593
raph-m <raphael.montaud@riminder.net>
2018-03-08 17:22:59 +0100

    duno

--- code/feature_engineering/networkx_bigraph_long.py
+++ code/feature_engineering/networkx_bigraph_long.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 import networkx as nx
+from multiprocessing import Pool
+from tqdm import tqdm
 
 from code.feature_engineering.tools import lit_eval_nan_proof
 
 # this script computes some features by considering the bidirectional graph of citations: katz
-# approx 1000 minutes to run it
+# approx 100 hours to run it
 
 # progress bar for pandas
 tqdm.pandas(tqdm())
@@ -24,10 +25,6 @@ training.set_index("my_index", inplace=True)
 testing = pd.read_csv(path_to_data + "testing_features.txt")
 testing.set_index("my_index", inplace=True)
 
-G = nx.Graph()
-G.add_nodes_from(nodes.index.values)
-G.add_edges_from(zip(training[training["target"] == 1]["id1"], training[training["target"] == 1]["id2"]))
-
 # IDs for training set
 id1 = training['id1'].values
 id2 = training['id2'].values
@@ -40,45 +37,16 @@ breaking_point = 10
 beta = 0.98
 beta_2 = 0.9
 
-# computing features for training set
-for i in tqdm(range(len(id1))):
-    if training.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
-        G.remove_edge(id1[i], id2[i])
 
-    katz = 0.0
-    katz_2 = 0.0
-    counter = 0
-    try:
-        iterator = nx.all_shortest_paths(G, source=id1[i], target=id2[i])
-        for p in iterator:
-            len_p = len(p)
-            katz += len_p * (beta ** len_p)
-            katz_2 += len_p * (beta_2 ** len_p)
-            counter += 1
-            if counter >= breaking_point:
-                break
-    except:
-        pass
+def work(i=None, id1=None, id2=None, df=None, training=True):
 
-    if training.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
-        G.add_edge(id1[i], id2[i])
+    G = nx.Graph()
+    G.add_nodes_from(nodes.index.values)
+    G.add_edges_from(zip(df[df["target"] == 1]["id1"], df[df["target"] == 1]["id2"]))
 
-# add feature to data-frame
-training["katz"] = katz
-
-
-# IDs for testing set
-id1 = testing['id1'].values
-id2 = testing['id2'].values
-
-# placeholder for feature
-n = len(id1)
-
-katz = np.zeros(n)
-katz_2 = np.zeros(n)
-
-# computing features for testing set
-for i in tqdm(range(len(id1))):
+    if training:
+        if df.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
+            G.remove_edge(id1[i], id2[i])
 
     katz_acc = 0.0
     katz_2_acc = 0.0
@@ -92,12 +60,60 @@ for i in tqdm(range(len(id1))):
             counter += 1
             if counter >= breaking_point:
                 break
-        katz[i] = katz_acc
-        katz_2[i] = katz_2_acc
     except:
         katz[i] = -1
         katz_2[i] = -1
-        pass
+
+    return katz_acc, katz_2_acc
+
+# computing features for training set
+pool = Pool()
+tasks = []
+for i in range(len(id1)):
+    kwds = {
+        "i": i,
+        "id1": id1,
+        "id2": id2,
+        "df": training,
+        "training": True
+    }
+    tasks.append(pool.apply_async(work, kwds=kwds))
+pool.close()
+pool.join()
+for i in range(len(katz)):
+    katz[i], katz_2[i] = tasks[i].get()
+
+# add feature to data-frame
+training["katz"] = katz
+training["katz_2"] = katz_2
+
+
+# IDs for testing set
+id1 = testing['id1'].values
+id2 = testing['id2'].values
+
+# placeholder for feature
+n = len(id1)
+
+katz = np.zeros(n)
+katz_2 = np.zeros(n)
+
+# computing features for testing set
+pool = Pool()
+tasks = []
+for i in range(len(id1)):
+    kwds = {
+        "i": i,
+        "id1": id1,
+        "id2": id2,
+        "df": training,
+        "training": True
+    }
+    tasks.append(pool.apply_async(work, kwds=kwds))
+pool.close()
+pool.join()
+for i in range(len(katz)):
+    katz[i], katz_2[i] = tasks[i].get()
 
 # add feature to data-frame
 testing["katz"] = katz
@@ -105,5 +121,5 @@ testing["katz_2"] = katz_2
 
 
 # save data-frame
-training.to_csv(path_to_data + "training_features.txt")
-testing.to_csv(path_to_data + "testing_features.txt")
+# training.to_csv(path_to_data + "training_features.txt")
+# testing.to_csv(path_to_data + "testing_features.txt")
