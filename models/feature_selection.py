1c8f6195f26e11afdf5f24af1cf92136b7fc4112
raph-m <raphael.montaud@riminder.net>
2018-03-09 01:44:39 +0100

    feature selection

--- models/feature_selection.py
+++ models/feature_selection.py
@@ -64,32 +64,58 @@ for i in range(len(training.columns)):
         my_features_index.append(i)
 
 features_to_keep = []
-for i, f in my_features_dic.items():
-    # separating features and labels
-    print("testing feature: " + f)
-    current_features = features_to_keep + [i]
+for u in range(len(my_features_index)):
+    features_to_keep_names = [my_features_dic[i] for i in features_to_keep]
+    print("new round !")
+    print("u = " + str(u) + ", current features are: " + str(features_to_keep_names))
+    best_test_score = 0.0
+    best_train_score = 0.0
+    best_index = 0
+    for i, f in my_features_dic.items():
+        # separating features and labels
+        print("testing additional feature: " + f)
+        current_features = features_to_keep + [i]
 
-    print(current_features)
+        print(current_features)
 
-    X_train = training.values[:, current_features].astype(float)
+        X_train = training.values[:, current_features].astype(float)
 
-    RF = RandomForestClassifier(
-        n_estimators=parameters["n_estimators"],
-        criterion=parameters["criterion"],
-        max_depth=parameters["max_depth"],
-        min_samples_leaf=parameters["min_samples_leaf"],
-        bootstrap=parameters["bootstrap"],
-        n_jobs=parameters["n_jobs"]
-    )
-    k = 2
-    kf = KFold(k)
+        RF = RandomForestClassifier(
+            n_estimators=parameters["n_estimators"],
+            criterion=parameters["criterion"],
+            max_depth=parameters["max_depth"],
+            min_samples_leaf=parameters["min_samples_leaf"],
+            bootstrap=parameters["bootstrap"],
+            n_jobs=parameters["n_jobs"]
+        )
+        k = 5
+        kf = KFold(k)
+        train_score = 0.0
+        test_score = 0.0
 
-    for train_index, test_index in kf.split(X_train, Y_train):
-        RF.fit(X_train[train_index], Y_train[train_index])
-        Y_pred = RF.predict(X_train[test_index])
-        Y_pred_train = RF.predict(X_train[train_index])
-        print("train: " + str(f1_score(Y_train[train_index], Y_pred_train)))
-        print("test: " + str(f1_score(Y_train[test_index], Y_pred)))
+        for train_index, test_index in kf.split(X_train, Y_train):
+            RF.fit(X_train[train_index], Y_train[train_index])
+            Y_pred = RF.predict(X_train[test_index])
+            Y_pred_train = RF.predict(X_train[train_index])
+            train_score += f1_score(Y_train[train_index], Y_pred_train)
+            test_score += f1_score(Y_train[test_index], Y_pred)
+
+        train_score /= k
+        test_score /= k
+
+        if test_score > best_test_score:
+            best_index = i
+            best_train_score = train_score
+
+        print("train score: "+str(train_score))
+        print("test score: " + str(test_score))
+        print("")
+
+    print("for this round, the best feature was " + my_features_dic[best_index])
+    print("the scores obtained were: ")
+    print("train score: " + str(best_train_score))
+    print("test score: " + str(best_test_score))
+    print("\n\n\n\n")
 
 # # print feature importances
 # for i in range(len(RF.feature_importances_)):
