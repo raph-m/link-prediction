9ba32ea702f5ec88516f2f5c257a6a84d30bdc89
raph-m <raphael.montaud@riminder.net>
2018-03-12 20:16:40 +0100

    new features

---
+++ models/authors_2.py
@@ -0,0 +1,183 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+from tqdm import tqdm
+
+from feature_engineering.tools import lit_eval_nan_proof
+
+# this script computes the features out_neighbors, in_neighbors and popularity by considering the directed
+# graph of citations. Popularity is the sum of in degrees of predecessors.
+# the script takes approximately 5 minutes to run
+
+# progress bar for pandas
+tqdm.pandas(tqdm())
+
+# path
+path_to_data = "data/"
+
+# loading data
+converter_dict = {'authors': lit_eval_nan_proof, 'journal': lit_eval_nan_proof,
+                  'title': lit_eval_nan_proof, 'abstract': lit_eval_nan_proof}
+nodes = pd.read_csv(path_to_data + "nodes_preprocessed.csv", converters=converter_dict)
+nodes.set_index("id", inplace=True)
+training = pd.read_csv(path_to_data + "training_features.txt")
+training.set_index("my_index", inplace=True)
+testing = pd.read_csv(path_to_data + "testing_features.txt")
+testing.set_index("my_index", inplace=True)
+
+# loading data
+converter_dict = {'authors': lit_eval_nan_proof, 'journal': lit_eval_nan_proof,
+                  'title': lit_eval_nan_proof, 'abstract': lit_eval_nan_proof}
+nodes = pd.read_csv(path_to_data + "nodes_preprocessed.csv",
+                    converters=converter_dict)
+nodes.set_index("id", inplace=True)
+
+G = nx.DiGraph()
+coauthors = nx.Graph()
+
+print("building coauthor graph")
+nodes_id = nodes.index.values
+for i in range(len(nodes_id)):
+
+    authors = nodes.loc[nodes_id[i]]["authors"]
+    if authors is np.nan:
+        authors = []
+
+    authors = [a for a in authors if a != ""]
+
+    for a in authors:
+        G.add_node(a)
+        coauthors.add_node(a)
+
+    for a1 in authors:
+        for a2 in authors:
+            if a1 != a2:
+                if coauthors.has_edge(a1, a2):
+                    coauthors[a1][a2]["weight"] += 1
+                else:
+                    coauthors.add_edge(a1, a2, weight=0)
+
+id1 = training["id1"].values
+id2 = training["id2"].values
+
+print("building citation graph")
+for i in range(len(id1)):
+    if i % 100000 == 0:
+        print(i)
+
+    current_authors_1 = nodes.loc[id1[i]]["authors"]
+    current_authors_2 = nodes.loc[id2[i]]["authors"]
+
+    if current_authors_1 is np.nan:
+        current_authors_1 = []
+
+    if current_authors_2 is np.nan:
+        current_authors_2 = []
+
+    current_authors_1 = [a for a in current_authors_1 if a != ""]
+    current_authors_2 = [a for a in current_authors_2 if a != ""]
+
+    if training.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
+        for a1 in current_authors_1:
+            for a2 in current_authors_2:
+                if G.has_edge(a1, a2):
+                    G[a1][a2]["weight"] += 1
+                else:
+                    G.add_edge(a1, a2, weight=0)
+
+
+authors_in_neighbors = np.zeros(len(id1))
+normalized_authors_in_neighbors = np.zeros(len(id1))
+best_authors_in_neighbors = np.zeros(len(id1))
+
+print("building features for training")
+for i in range(len(id1)):
+    if i % 100000 == 0:
+        print(i)
+    current_authors_1 = nodes.loc[id1[i]]["authors"]
+    current_authors_2 = nodes.loc[id2[i]]["authors"]
+
+    if current_authors_1 is np.nan:
+        current_authors_1 = []
+
+    if current_authors_2 is np.nan:
+        current_authors_2 = []
+
+    current_authors_1 = [a for a in current_authors_1 if a != ""]
+    current_authors_2 = [a for a in current_authors_2 if a != ""]
+
+    if training.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
+        for a1 in current_authors_1:
+            for a2 in current_authors_2:
+                G[a1][a2]["weight"] -= 1
+
+    best = 0
+    for a1 in current_authors_2:
+        current = len(G.predecessors(a1))
+        authors_in_neighbors[i] += current
+        if current > best:
+            best = current
+
+    best_authors_in_neighbors[i] = best
+
+    denom = len(current_authors_2)
+    if denom > 0:
+        normalized_authors_in_neighbors[i] = authors_in_neighbors[i] / denom
+
+    if training.at[str(id1[i]) + "|" + str(id2[i]), "target"] == 1:
+        for a1 in current_authors_1:
+            for a2 in current_authors_2:
+                G[a1][a2]["weight"] += 1
+
+training["authors_in_neighbors"] = authors_in_neighbors
+training["normalized_authors_in_neighbors"] = normalized_authors_in_neighbors
+training["best_authors_in_neighbors"] = best_authors_in_neighbors
+
+id1 = testing["id1"].values
+id2 = testing["id2"].values
+
+coauthor_score = np.zeros(len(id1))
+normalized_coauthor_score = np.zeros(len(id1))
+best_coauthor_score = np.zeros(len(id1))
+authors_citation = np.zeros(len(id1))
+normalized_authors_citation = np.zeros(len(id1))
+best_authors_citation = np.zeros(len(id1))
+
+print("building features for testing")
+for i in range(len(id1)):
+    if i % 100000 == 0:
+        print(i)
+    current_authors_1 = nodes.loc[id1[i]]["authors"]
+    current_authors_2 = nodes.loc[id2[i]]["authors"]
+
+    if current_authors_1 is np.nan:
+        current_authors_1 = []
+
+    if current_authors_2 is np.nan:
+        current_authors_2 = []
+
+    current_authors_1 = [a for a in current_authors_1 if a != ""]
+    current_authors_2 = [a for a in current_authors_2 if a != ""]
+
+    best = 0
+    for a1 in current_authors_2:
+        current = len(G.predecessors(a1))
+        authors_in_neighbors[i] += current
+        if current > best:
+            best = current
+
+    best_authors_in_neighbors[i] = best
+
+    denom = len(current_authors_2)
+    if denom > 0:
+        normalized_authors_in_neighbors[i] = authors_in_neighbors[i] / denom
+
+testing["authors_in_neighbors"] = authors_in_neighbors
+testing["normalized_authors_in_neighbors"] = normalized_authors_in_neighbors
+testing["best_authors_in_neighbors"] = best_authors_in_neighbors
+
+print("done, saving data")
+# save data-frame
+training.to_csv(path_to_data + "training_features.txt")
+testing.to_csv(path_to_data + "testing_features.txt")
+
